ac02a58d88cb3273755792188f4b14c206da4441
Nostalia17 <155804556+Nostalia17@users.noreply.github.com>
2024-03-08 11:27:28 +0800

    Update script.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,6 +3,6 @@ document.getElementById('launchButton').addEventListener('click', function(event
     document.body.classList.add('fadeout'); // Add the 'fadeout' class to the body
 
     setTimeout(function() {
-        window.location.href = '../docs/newpage.html'; // Redirect to newPage.html after 6 seconds
+        window.location.href = '../docs/newPage.html'; // Redirect to newPage.html after 6 seconds
     }, 6000); // Change 3000 to 6000
 });
